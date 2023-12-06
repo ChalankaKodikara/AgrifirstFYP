30a8bcc37784b1c1ddeb8039bf01a39fd4257ef8
chalankakodikara <ischalankakodikara@gmail.com>
2023-12-06 15:37:41 +0530

    forum page created

--- frontend/src/components/DiseaseDetection/ImageUpload.js
+++ frontend/src/components/DiseaseDetection/ImageUpload.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./ImageUpload.css";
+import Loader from "./loder/Loder";
 
 const ImageUpload = () => {
   const [file, setFile] = useState(null);
   const [prediction, setPrediction] = useState(null);
   const [treatment, setTreatment] = useState(null);
   const [pdfPath, setPdfPath] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -14,12 +17,14 @@ const ImageUpload = () => {
   const handleUpload = async () => {
     if (!file) return;
 
+    setIsLoading(true); // Set loading to true before making the request
+
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
 
     try {
-      const response = await fetch('http://localhost:5000/predict', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5000/predict", {
+        method: "POST",
         body: formData,
       });
       const data = await response.json();
@@ -27,28 +32,42 @@ const ImageUpload = () => {
       setTreatment(data.treatment);
       setPdfPath(data.pdf_path);
     } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
-  const handleDownload = () => {
-    if (pdfPath) {
-      window.open(`/download-pdf/${pdfPath}`);
+      console.error("Error:", error);
+    } finally {
+      setIsLoading(false); // Set loading to false after the request is complete
     }
   };
 
   return (
     <div>
-      <h1>Upload an Image</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
+      <div class="heroSection">
+        <div class="heroText"></div>
+      </div>
+      <div className="formpage">
+        <br />
+        <form class="form">
+          <span class="form-title">Upload your file</span>
+          <p class="form-paragraph">File should be an image</p>
+          <label for="file-input" class="drop-container">
+            <span class="drop-title">Drop files here</span>
+            or
+            <input
+              className="file-input"
+              type="file"
+              accept="image/*"
+              required=""
+              id="file-input"
+              onChange={handleFileChange}
+            />
+          </label>
+          <button className="uploadbtn" id="file-input" onClick={handleUpload}>
+            Upload
+          </button>
+        </form>
+      </div>
+      {isLoading && <Loader />} {/* Render loader when loading is true */}
       {prediction && <h2>Prediction: {prediction}</h2>}
       {treatment && <h2>Treatment: {treatment}</h2>}
-      {pdfPath && (
-        <button onClick={handleDownload}>
-          Download PDF Report
-        </button>
-      )}
     </div>
   );
 };
---
+++ frontend/src/components/DiseaseDetection/loder/Loder.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Loder.css'
+
+function Loder() {
+  return (
+    <div className="loadercom">
+      <div class="loader">
+        <div class="cell d-0"></div>
+        <div class="cell d-1"></div>
+        <div class="cell d-2"></div>
+
+        <div class="cell d-1"></div>
+        <div class="cell d-2"></div>
+
+        <div class="cell d-2"></div>
+        <div class="cell d-3"></div>
+
+        <div class="cell d-3"></div>
+        <div class="cell d-4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loder;

27e8adb8ba9f5b3d5d389f31b59283ee3f43de8b
chalankakodikara <ischalankakodikara@gmail.com>
2023-12-07 17:14:06 +0530

    bug fixed

--- frontend/src/components/DiseaseDetection/ImageUpload.js
+++ frontend/src/components/DiseaseDetection/ImageUpload.js
@@ -6,7 +6,6 @@ const ImageUpload = () => {
   const [file, setFile] = useState(null);
   const [prediction, setPrediction] = useState(null);
   const [treatment, setTreatment] = useState(null);
-  const [pdfPath, setPdfPath] = useState(null);
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -27,7 +26,6 @@ const ImageUpload = () => {
       const data = await response.json();
       setPrediction(data.prediction);
       setTreatment(data.treatment);
-      setPdfPath(data.pdf_path);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -35,61 +33,60 @@ const ImageUpload = () => {
 
   return (
     <div>
-      <div class="heroSection">
-        <div class="heroText"></div>
+      <div className="heroSection">
+        <div className="heroText"></div>
       </div>
       <div className="formpage">
-        <div className="formpage">
-          <br />
-          <form class="formu">
-            <span class="form-title">Upload your file</span>
-            <p class="form-paragraph">File should be an image</p>
-            <label for="file-input" class="drop-container">
-              <span class="drop-title">Drop files here</span>
-              or
-              <input
-                className="file-input"
-                type="file"
-                accept="image/*"
-                required=""
-                id="file-input"
-                onChange={handleFileChange}
-              />
-            </label>
-            
-          </form>
-        </div>
+        <br />
+        <form className="formu">
+          <span className="form-title">Upload your file</span>
+          <p className="form-paragraph">File should be an image</p>
+          <label htmlFor="file-input" className="drop-container">
+            <span className="drop-title">Drop files here</span>
+            or
+            <input
+              className="file-input"
+              type="file"
+              accept="image/*"
+              required=""
+              id="file-input"
+              onChange={handleFileChange}
+            />
+          </label>
+        </form>
       </div>
-      <div className="buttonpr">
-        <button className="uploadbtn" id="file-input" onClick={handleUpload}>
+      <div className="formpage">
+        <button className="buttonrg" onClick={handleUpload}>
           Upload
         </button>
       </div>
       <div className="prediction ">
-        <div class="notifications-container">
-          <div class="success">
-            <div class="flex">
-              <div class="flex-shrink-0">
+        <div className="notifications-container">
+          <div className="success">
+            <div className="flex">
+              <div className="flex-shrink-0">
                 <svg
-                  class="succes-svg"
+                  className="succes-svg"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
                   ></path>
                 </svg>
               </div>
-              <div class="success-prompt-wrap">
-                <p class="success-prompt-heading">
-                  {prediction && <h2>Prediction: {prediction}</h2>}
-                </p>
-                <div class="success-prompt-prompt">
-                  <p>{treatment && <h2>Treatment: {treatment}</h2>}</p>
+              <br />
+              <div className="successpg">
+                <div className="success-prompt-wrap">
+                  <p className="success-prompt-heading">
+                    {prediction && <h2>Prediction: {prediction}</h2>}
+                  </p>
+                  <div className="success-prompt-prompt">
+                    <p>{treatment && <h2>Treatment: {treatment}</h2>}</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -99,4 +96,5 @@ const ImageUpload = () => {
     </div>
   );
 };
+
 export default ImageUpload;

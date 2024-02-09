94d144b424ae05d46b9ce9ba001dc189f33c6cec
Chalanka Kodikara <ischalankakodikara@gmail.com>
2024-02-09 11:06:30 +0530

    notificaion addedccc

--- frontend/src/components/DiseaseDetection/ImageUpload.js
+++ frontend/src/components/DiseaseDetection/ImageUpload.js
@@ -60,39 +60,64 @@ const ImageUpload = () => {
           Upload
         </button>
       </div>
-      <div className="prediction ">
-        <div className="notifications-container">
-          <div className="success">
-            <div className="flex">
-              <div className="flex-shrink-0">
-                <svg
-                  className="succes-svg"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                  ></path>
-                </svg>
-              </div>
-              <br />
-              <div className="successpg">
-                <div className="success-prompt-wrap">
-                  <p className="success-prompt-heading">
-                    {prediction && <h2>Prediction: {prediction}</h2>}
-                  </p>
-                  <div className="success-prompt-prompt">
-                    <p>{treatment && <h2>Treatment: {treatment}</h2>}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div
+        className="prediction"
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
+        {prediction && treatment && (
+          <div className="notifications-container">
+            {" "}
+            <div className="success">
+              {" "}
+              <div className="flex">
+                {" "}
+                <div className="flex-shrink-0">
+                  {" "}
+                  <svg
+                    className="succes-svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    {" "}
+                    <></>
+                    <div></div>
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    ></path>{" "}
+                  </svg>{" "}
+                </div>{" "}
+                <br />{" "}
+                <div className="successpg">
+                  {" "}
+                  <div className="success-prompt-wrap">
+                    {" "}
+                    <p className="success-prompt-heading">
+                      {" "}
+                      <h2>Prediction: {prediction}</h2>{" "}
+                    </p>{" "}
+                    <div className="success-prompt-prompt">
+                      {" "}
+                      <p>
+                        {" "}
+                        <h2>Treatment: {treatment}</h2>{" "}
+                      </p>{" "}
+                    </div>{" "}
+                  </div>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
           </div>
-        </div>
-      </div>{" "}
+        )}{" "}
+      </div>
     </div>
   );
 };
